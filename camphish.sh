8afb920e1b6a54696ce22b2a9761c27a21b938e5
white-stone-12 <jack654sparrow@gmail.com>
2025-05-26 05:41:52 -0400

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -39,7 +39,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
 printf " \e[1;93m CamPhish Ver 2.0 \e[0m \n"
-printf " \e[1;77m www.techchip.net | youtube.com/techchipnet \e[0m \n"
+printf " \e[1;77m JAGA CAMPHISH\e[0m \n"
 
 printf "\n"
 
